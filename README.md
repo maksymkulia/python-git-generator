ff93618b9f1d37ccd7f04118752f6e6c49513d91
Maksym Kulia <maksym.kulia@protonmail.com>
2020-12-07 22:40:51 +0200

    [add] working MVP
    Signed-off-by: Maksym Kulia <maksym.kulia@protonmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Git repository generator
 This is a simple git repository generator on Python.
 
+_Please note, it is proto which is poorly tested._
+
 ## Usage
 - Download and install [Python](https://www.python.org/downloads/)
 - Run ``python3 src/main.py``
--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
 # Importing modules
 import os
 
+# Providing params
+name = input("Name of reposiory: ")
+parent_dir = input("Provide path (without name of repo): ")
+
+path = os.path.join(parent_dir, name)
+os.mkdir(path)
+
+print("Empty project created")
+
+# Creating files
+readme = "README.md"
+f = open(os.path.join(path, readme), "w")
+f.write("# " + name)
+f.close()
+
+# Creating git repo
+os.chdir(path)
+os.system("git init")
